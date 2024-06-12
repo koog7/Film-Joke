27048b6cf758c6cfbeb425455c58cc8c7322a55d
koog7 <k.ognev709@mail.com>
2024-06-12 13:14:09 +0600

    Разработан функционал для поÐявления шутки ÑЧака ÐНориса и кнопка обновÐления

--- src/components/ChuckNorris.tsx
+++ src/components/ChuckNorris.tsx
@@ -1,8 +1,42 @@
-const ChuckNorris = () => {
-    return (
-        <div>
+import {useEffect, useState} from "react";
 
-        </div>
+interface jokeProps{
+    value: string;
+}
+
+const ChuckNorris = () => {
+    const [data , setData] = useState<jokeProps>();
+    const url = 'https://api.chucknorris.io/jokes/random';
+
+    const fetchData = async () => {
+        const response = await fetch(url);
+        if (response.ok) {
+            const joke = await response.json();
+            setData({value: joke.value});
+        }
+    };
+
+    useEffect(() => {
+        void fetchData();
+    }, []);
+
+    console.log(data?.value)
+
+    const btnClick = () => {
+        setData({ value: '' });
+        void fetchData();
+    };
+
+    return (
+        <fieldset className="card">
+            <legend>Chuck Norris daily joke</legend>
+            <div className="card-body w-100 d-flex">
+                <p>{data?.value}</p>
+                <div className={'ms-auto'}>
+                    <button className={'ms-auto btn btn-success'} onClick={btnClick}>Roll</button>
+                </div>
+            </div>
+        </fieldset>
     );
 };
 
